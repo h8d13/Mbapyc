b29123f54f5d772b62cc4c13479eb01ef58364f2
h8d13 <hadrien.loge@gmail.com>
2025-11-05 17:41:48 +0100

    mod code

--- tmpiler.py
+++ tmpiler.py
@@ -8,6 +8,7 @@ import shutil
 tmp = tf.mkdtemp(dir=os.getcwd())
 
 def _read_code(c_code):
+    ## Simply read the existing file called in myscript
     try:
         with open(c_code, "r") as f:
             return f.read()
@@ -20,10 +21,15 @@ def _read_code(c_code):
         sus.exit(1)
 
 def _tmp_code(c_path, c_code):
-    """Write C code to a temp file and return the path."""
+    ## Write C code to a temp file and return the path.
     with open(c_path, "w") as f:
         f.write(c_code)
     return c_path
+
+def _mod_code(c_code):
+    ## Using our temp strat we can modify on the fly existing bitcount.c file
+    new_max = 8
+    c_code = c_code.replace("const int MAX_BITS = 32;", f"const int MAX_BITS = {new_max};")
 
 def _tmpile_c(c_code, work_dir=None, so_name="bitcount.so", flags=None):
     """
@@ -36,6 +42,8 @@ def _tmpile_c(c_code, work_dir=None, so_name="bitcount.so", flags=None):
     tmp_dir = tf.mkdtemp(dir=work_dir)
     c_path = os.path.join(tmp_dir, "bitcount.c")
     exe_path = os.path.join(tmp_dir, so_name)
+
+    #_mod_code(c_code)
 
     _tmp_code(c_path, c_code)
 
